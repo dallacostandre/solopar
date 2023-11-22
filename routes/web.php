0861070eb39207178c4fd6e499730664ad0fdd26
Andre <dallacosta.andre@gmail.com>
2023-11-22 11:55:59 -0300

    envio do email para hotmail

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::post('/email', function (Request $request) {
         function ($m) {
             $m->from('contato@soloparfundacoes.com.br', 'Solopar Site');
             $m->to('soloparfundacoes@hotmail.com.br');
-            $m->subject('Novo Solicitacao via Site');
+            $m->subject('Novo Contato via Site');
         }
     );
     // return redirect('/')->with('success', 'Mensagem enviada com sucesso!');
